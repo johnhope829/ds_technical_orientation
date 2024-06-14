17dcf7b21945735a697b1c3bbaf91fa719f03516
John Hope <johnhope829@gmail.com>
2024-06-14 16:23:42 -0400

    change

--- analysis.R
+++ analysis.R
@@ -5,8 +5,8 @@ df <- read_csv("data/MSDS-Orientation-Computer-Survey.csv")
 
 # plot
 df %>%
-  filter(RAM < 64) %>%
+  filter(RAM < 64, format(as.Date(df$Timestamp, format = "%m/%d/%Y"), "%Y") == "0024") %>%
   ggplot(aes(x=RAM, fill = os)) +
   geom_histogram(binwidth=4, fill="#69b3a2", color="#e9ecef", alpha=0.9) +
-  ggtitle("Count of Computers by RAM (in GB)") +
+  ggtitle("2024 Cohort Count of Computers by RAM (in GB)") +
   labs(x = 'RAM (in GB)', y = 'Count')
