ed87497314f40e06822632b1129f0e115392d27f
John Hope <johnhope829@gmail.com>
2024-06-14 16:15:17 -0400

    r file

---
+++ analysis.R
@@ -0,0 +1,12 @@
+library(tidyverse)
+library(ggplot2)
+
+df <- read_csv("data/MSDS-Orientation-Computer-Survey.csv")
+
+# plot
+df %>%
+  filter(RAM < 64) %>%
+  ggplot(aes(x=RAM, fill = os)) +
+  geom_histogram(binwidth=4, fill="#69b3a2", color="#e9ecef", alpha=0.9) +
+  ggtitle("Count of Computers by RAM (in GB)") +
+  labs(x = 'RAM (in GB)', y = 'Count')
